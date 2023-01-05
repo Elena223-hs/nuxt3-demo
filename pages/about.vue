82fc09994ef14a67000d2faaab2b30fe572453f1
Elena <elenaanakievska1@gmail.com>
2023-01-05 13:18:03 +0200

    navigate

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="bg-red text-3xl font-bold text-white underline">
+  <div>
+    <NuxtLink to="/">
+      Home
+    </NuxtLink>
+    <NuxtLink to="about">
+      About
+    </NuxtLink>
     Hello About
   </div>
-  <div class="i-carbon-moon w-10 h-10 " />
 </template>
