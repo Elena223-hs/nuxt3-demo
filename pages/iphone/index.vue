d0de9460588f5bd5ebda01bf5bca67115b807762
Elena <elenaanakievska1@gmail.com>
2023-01-05 17:04:36 +0200

    iphones variants

---
+++ pages/iphone/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>
+      <h1>IPhone 12</h1>
+      <img width="150" src="/images/iphone12_.webp" alt="">
+      <h1>IPhone 12 Pro</h1>
+      <img width="150" src="/images/iphone12pro_.webp" alt="">
+    </div>
+  </div>
+</template>
