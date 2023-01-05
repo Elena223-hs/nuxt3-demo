953931d91009b2f8d600a75aed5dbaf169b3b0a5
Elena <elenaanakievska1@gmail.com>
2023-01-05 13:17:48 +0200

    navigate pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
+<script setup>
+function moveToAbout() {
+  navigateTo('about')
+}
+</script>
+
 <template>
-  Hello index
+  <div>
+    <NuxtLink to="/">
+      Home
+    </NuxtLink>
+    <NuxtLink to="about">
+      About
+    </NuxtLink>
+    <button @click="moveToAbout">
+      Take me to About Page
+    </button>
+    Hello Index
+  </div>
 </template>
